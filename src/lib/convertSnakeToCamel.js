91975b895090e28639d2072a2b7bfff8105d5277
donggeun <geeneve@naver.com>
2022-04-25 23:19:23 +0900

    feat: 문자열 변환 메서드 추가
    snake to camel, camel to snake

--- src/lib/convertSnakeToCamel.js
+++ src/lib/convertSnakeToCamel.js
@@ -49,7 +49,20 @@ const keysToSnake = function (o) {
   return o;
 };
 
+const toSnakeString = (e) => {
+  return e
+    .match(/([A-Z])/g)
+    .reduce((str, c) => str.replace(new RegExp(c), '_' + c.toLowerCase()), e)
+    .substring(e.slice(0, 1).match(/([A-Z])/g) ? 1 : 0);
+};
+
+const toCamelString = (e) => {
+  return e.replace(/_([a-z])/g, (g) => g[1].toUpperCase());
+};
+
 module.exports = {
   keysToCamel,
   keysToSnake,
+  toSnakeString,
+  toCamelString,
 };
