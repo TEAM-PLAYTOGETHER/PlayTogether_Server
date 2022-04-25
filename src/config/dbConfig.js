18f5b227d684daa4e1e0bee5dfa72341f7eae66f
donggeun <geeneve@naver.com>
2022-04-26 00:46:16 +0900

    chore: dbConfig 배포용 주석 해제
    production 관련 주석 해제

--- src/config/dbConfig.js
+++ src/config/dbConfig.js
@@ -8,13 +8,11 @@ module.exports = {
     database: process.env.DB_DB_DEV,
     password: process.env.DB_PASSWORD,
   },
-  /* 
-  // 추후에 배포 단계에서 다른 DB 사용할 것이라면 추가하기
   production: {
     user: process.env.DB_USER,
     host: process.env.DB_HOST,
-    database: process.env.DB_DB_PROD,
+    // database: process.env.DB_DB_PROD,
+    database: process.env.DB_DB_DEV,
     password: process.env.DB_PASSWORD,
   },
-  */
 };
