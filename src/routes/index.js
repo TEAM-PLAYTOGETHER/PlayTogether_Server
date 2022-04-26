70cb0a9d13bcd5d724863fa312e79e23821d1e86
donggeun <geeneve@naver.com>
2022-04-27 01:08:38 +0900

    feat: POST /message 라우트 추가
    메시지 전송 라우트 추가

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,5 +3,6 @@ const express = require('express');
 const router = express.Router();
 
 router.use('/test', require('./testAPI'));
+router.use('/message', require('./messageAPI'));
 
 module.exports = router;
