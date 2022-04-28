0ed5126c5add14198ffcc016774dd7fc99901cf2
LeeJejune <leejejune0625@gmail.com>
2022-04-28 19:08:36 +0900

    feat: 번개 수정 controller 구현

--- src/controller/lightController.js
+++ src/controller/lightController.js
@@ -29,6 +29,22 @@ const addLight = async (req, res) => {
 };
 
 
+const putLight = async (req, res) => {
+  const { lightId } = req.params;
+  const { category, title, date, place, people_cnt, description, time  } = req.body;
+
+  let client;
+  
+  if (!lightId) return res.status(statusCode.BAD_REQUEST).send(util.fail(statusCode.BAD_REQUEST, responseMessage.NULL_VALUE));
+
+  try {
+    client = await db.connect(req);
+    const updatedPost = await lightService.putLight(client, lightId, category, title, date, place,
+      people_cnt, description, time);
+
+    if (!updatedPost) return res.status(statusCode.NOT_FOUND).send(util.fail(statusCode.NOT_FOUND, responseMessage.NO_POST));
+      
+    return res.status(statusCode.OK).json(util.success(statusCode.OK, responseMessage.LIGHT_PUT_SUCCESS));
   } catch (error) {
     console.log(error);
     return res.status(statusCode.INTERNAL_SERVER_ERROR).json(util.fail(statusCode.INTERNAL_SERVER_ERROR, responseMessage.INTERNAL_SERVER_ERROR));
@@ -37,6 +53,9 @@ const addLight = async (req, res) => {
   }
 };
 
+
+
 module.exports = {
     addLight,
+    putLight
 };
