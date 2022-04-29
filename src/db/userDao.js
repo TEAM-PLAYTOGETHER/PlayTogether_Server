ec736e4c79af3a225a916e2f2c9f947029c5438b
Crayon <lsh328328@naver.com>
2022-04-30 03:25:45 +0900

    feat: 로그인 API 구현
    로그인 API 구현
    jwt 관련 메서드 구현
    
    Related to: #20

--- src/db/userDao.js
+++ src/db/userDao.js
@@ -12,9 +12,25 @@ const getUserById = async (userId) => {
     `
       SELECT *
       FROM "user"
-      WHERE user_login_id = $1
+      WHERE id = $1
       `,
     [userId],
+  );
+  client.release();
+  return convertSnakeToCamel.keysToCamel(rows[0]);
+};
+
+const getUserByUserLoginId = async (userLoginId) => {
+  const client = await db.connect('로그');
+
+  // 유저 아이디로 유저 조회 쿼리문
+  const { rows } = await client.query(
+    `
+      SELECT *
+      FROM "user"
+      WHERE user_login_id = $1
+      `,
+    [userLoginId],
   );
   client.release();
   return convertSnakeToCamel.keysToCamel(rows[0]);
@@ -26,4 +42,5 @@ const getUserById = async (userId) => {
 
 module.exports = {
   getUserById,
+  getUserByUserLoginId,
 };
--- src/service/authService.js
+++ src/service/authService.js
@@ -3,10 +3,11 @@ const statusCode = require('../constants/statusCode');
 const { userDao, authDao } = require('../db');
 const util = require('../lib/util');
 const bcrypt = require('bcrypt');
+const { jwtGenerator } = require('../lib/jwtHandler');
 
-const createUser = async (userId, password, userName, gender, birth, mbti) => {
+const createUser = async (userLoginId, password, userName, gender, birth, mbti) => {
   // 이미 존재하는 유저인지 확인
-  const isUser = await userDao.getUserById(userId);
+  const isUser = await userDao.getUserByUserId(userLoginId);
 
   try {
     if (isUser) {
@@ -16,7 +17,7 @@ const createUser = async (userId, password, userName, gender, birth, mbti) => {
     // bcrypt를 이용한 비밀번호 암호화
     const encryptedPassword = bcrypt.hashSync(password, 10);
 
-    const newUser = await authDao.createUser(userId, encryptedPassword, userName, gender, birth, mbti);
+    const newUser = await authDao.createUser(userLoginId, encryptedPassword, userName, gender, birth, mbti);
 
     return util.success(statusCode.OK, responseMessage.CREATED_USER, newUser);
   } catch (error) {
@@ -25,6 +26,33 @@ const createUser = async (userId, password, userName, gender, birth, mbti) => {
   }
 };
 
+const login = async (userLoginId, password) => {
+  // 유저가 존재하는지 확인
+  const isUser = await userDao.getUserByUserLoginId(userLoginId);
+
+  try {
+    if (!isUser) {
+      return util.fail(statusCode.NOT_FOUND, responseMessage.NO_USER);
+    }
+
+    // bcrypt로 암호화된 비밀번호 비교
+    const passwordCheck = bcrypt.compareSync(password, isUser.password);
+
+    if (!passwordCheck) {
+      return util.fail(statusCode.UNAUTHORIZED, responseMessage.MISS_MATCH_PW);
+    }
+
+    // jwt 토큰 생성
+    const jwtToken = jwtGenerator(isUser);
+
+    return util.success(statusCode.OK, responseMessage.LOGIN_SUCCESS, { userLoginId: isUser.userLoginId, userName: isUser.name, jwtToken });
+  } catch (error) {
+    console.log('authService login에서 error 발생: ' + error);
+    return util.fail(statusCode.INTERNAL_SERVER_ERROR, responseMessage.INTERNAL_SERVER_ERROR);
+  }
+};
+
 module.exports = {
   createUser,
+  login,
 };
