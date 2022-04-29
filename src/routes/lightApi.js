032907c8c8b4aea8d5508ac7c86c5eaf11e01786
donggeun <geeneve@naver.com>
2022-04-30 03:06:12 +0900

    style: lightController 오타 수정

--- src/routes/lightApi.js
+++ src/routes/lightApi.js
@@ -1,10 +1,10 @@
 const express = require('express');
-const { lightConroller } = require('../controller');
+const { lightController } = require('../controller');
 const upload = require('../middlewares/multer');
 
 const router = express.Router();
 
-router.post('/add/:crewId/:organizerId', upload.single('image'), lightConroller.addLight);
-router.put('/:lightId', lightConroller.putLight);
+router.post('/add/:crewId/:organizerId', upload.single('image'), lightController.addLight);
+router.put('/:lightId', lightController.putLight);
 
 module.exports = router;
