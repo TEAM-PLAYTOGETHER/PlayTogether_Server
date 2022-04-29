0e87596452dd9c23a9f3889df760cd93319e7f08
donggeun <geeneve@naver.com>
2022-04-30 03:07:16 +0900

    feat: crewDao.getCrewByCode 쿼리 추가
    코드를 통해 동아리 정보 가져오기
    
    #14

--- src/db/crewDao.js
+++ src/db/crewDao.js
@@ -6,19 +6,41 @@ const createCrew = async (name, code, masterId) => {
   let client;
   const log = `crewDao.createCrew | name = ${name}, code=${code}, masterId=${masterId}`;
   try {
-    client = db.connect();
+    client = await db.connect(log);
 
     const { rows } = await client.query(
       `
-            insert into crew (name, code, master_id)
-            values ($1, $2, $3)
-            returning id, name, code
+        insert into crew (name, code, master_id)
+        values ($1, $2, $3)
+        returning id, name, code
             `,
       [name, code, masterId],
     );
     return convertSnakeToCamel.keysToCamel(rows[0]);
   } catch (error) {
-    console.log(log + '에서 오류 발생');
+    console.log(log + '에서 오류 발생' + error);
+    return null;
+  } finally {
+    client.release();
+  }
+};
+
+const getCrewByCode = async (code) => {
+  let client;
+  const log = `crewDao.getCrewByCode | code = ${code}`;
+  try {
+    client = await db.connect(log);
+
+    const { rows } = await client.query(
+      `
+        select * from crew
+        where code = $1
+            `,
+      [code],
+    );
+    return convertSnakeToCamel.keysToCamel(rows[0]);
+  } catch (error) {
+    console.log(log + '에서 오류 발생' + error);
     return null;
   } finally {
     client.release();
@@ -27,4 +49,5 @@ const createCrew = async (name, code, masterId) => {
 
 module.exports = {
   createCrew,
+  getCrewByCode,
 };
