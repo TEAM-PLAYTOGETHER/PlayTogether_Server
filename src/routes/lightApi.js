49ba2e2c487f26c8844a1ae97ebb9ef7b4e07da8
LeeJejune <leejejune0625@gmail.com>
2022-04-30 15:03:53 +0900

    refactor: lightApi 오타 수정

--- src/routes/lightApi.js
+++ src/routes/lightApi.js
@@ -4,8 +4,8 @@ const upload = require('../middlewares/multer');
 
 const router = express.Router();
 
-router.post('/add/:crewId/:organizerId', upload.single('image'), lightConroller.addLight);
-router.put('/:lightId', lightConroller.putLight);
-router.post('/enter/:lightId/:memberId', lightConroller.postEnterLight);
+router.post('/add/:crewId/:organizerId', upload.single('image'), lightController.addLight);
+router.put('/:lightId', lightController.putLight);
+router.post('/enter/:lightId/:memberId', lightController.postEnterLight);
 
 module.exports = router;
