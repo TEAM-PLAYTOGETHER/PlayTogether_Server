d1a8bbd51a69aa1b8d60ba7c5a402d29cc440ab3
donggeun <geeneve@naver.com>
2022-08-15 15:54:06 +0900

    feat: 소켓 서버 추가

--- src/index.js
+++ src/index.js
@@ -9,6 +9,10 @@ const passportConfig = require('./passport');
 const passport = require('passport');
 const session = require('express-session');
 const redis = require('redis');
+const { Server } = require('socket.io');
+const { messageService } = require('./service');
+const { messageController } = require('./controller');
+
 // const sentry = require('@sentry/node');
 // const tracing = require('@sentry/tracing');
 
@@ -94,5 +98,53 @@ const server = app
     console.error(err);
     process.exit(1);
   });
+
+const io = new Server(server);
+io.on('connection', (socket) => {
+  // 클라가 io 서버 접속시 고유 id를 얻음
+  console.log(`Connection : SocketId = ${socket.id}`);
+
+  // 1. 헤더 확인 -> 서버 연결 필요
+  const jwt = socket.handshake.headers.authorization;
+
+  // 2. 방 번호 확인
+  let roomId;
+  let audienceId;
+
+  socket.on('subscribe', (data) => {
+    const parsedData = JSON.parse(data);
+    roomId = parsedData.roomId;
+    audienceId = parsedData.audienceId;
+
+    socket.join(roomId);
+    console.log(`${socket.id}님이 ${roomId}에 접속: 대화상대 = ${audienceId}`);
+  });
+
+  socket.on('sendMessage', async (data) => {
+    const parsedData = JSON.parse(data);
+    const chatData = {
+      recvId: audienceId,
+      content: parsedData.messageContent,
+    };
+
+    console.log(chatData);
+
+    socket.broadcast.to(roomId).emit('newMessage', JSON.stringify(chatData));
+
+    // DB에 메시지 저장
+    try {
+      console.log(data);
+    } catch (error) {
+      console.log(error);
+    }
+
+    // 푸시알림 이벤트 트리거
+  });
+
+  socket.on('disconnect', () => {
+    socket.leave(roomId);
+    console.log('disconnect 합니다.');
+  });
+});
 
 server.timeout = 1000000;
