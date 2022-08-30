eb318f5ff741c719c5ba7485ad385bc4b2632947
LeeJejune <leejejune0625@gmail.com>
2022-08-30 23:27:18 +0900

    fix: 미친 프리티어 기능 발견

--- src/controller/lightController.js
+++ src/controller/lightController.js
@@ -7,7 +7,7 @@ const responseMessage = require('../constants/responseMessage');
 const addLight = async (req, res, next) => {
   const organizerId = req.user.id;
   const { crewId } = req.params;
-  let image = "";
+  let image = '';
   if (req.file) {
     image = req.file.location;
   }
@@ -30,7 +30,7 @@ const addLight = async (req, res, next) => {
 };
 const putLight = async (req, res, next) => {
   const organizerId = req.user.id;
-  var image = null;
+  let image = '';
   if (req.file) {
     image = req.file.location;
   }
@@ -240,7 +240,7 @@ const ExistLightUser = async (req, res, next) => {
   const { lightId } = req.params;
 
   if (!memberId) return res.status(statusCode.BAD_REQUEST).send(util.fail(statusCode.BAD_REQUEST, responseMessage.NULL_VALUE));
-  
+
   if (!lightId) return res.status(statusCode.BAD_REQUEST).send(util.fail(statusCode.BAD_REQUEST, responseMessage.NO_LIGHT));
 
   try {
@@ -265,5 +265,5 @@ module.exports = {
   getNewLight,
   getHotLight,
   getSearchLight,
-  ExistLightUser
+  ExistLightUser,
 };
